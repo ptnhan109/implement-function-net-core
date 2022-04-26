9cb5c45ee59d5f43b54d921f09c35e04351fbca5
Pham Trong Nhan <ptnhan109@gmail.com>
2022-04-26 22:37:55 +0700

    generate barcode with Zxing package.

---
+++ BarCode/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using BarCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BarCode.Helper;
+using System.IO;
+namespace BarCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GenerateBarCode(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                return Json(false);
+            }
+
+            await BarCodeHelper.GenerateBarCode(data);
+
+            return Json(true);
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> PrintBarCode()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "print-barcode-template.html");
+            string content = System.IO.File.ReadAllText(path);
+            var replace = new System.Text.StringBuilder();
+            for(int i = 0; i < 60 ; i++)
+            {
+                replace.Append(@"<img src='/barcode.png' class='item' />");
+            }
+
+            content = content.Replace("{content}", replace.ToString());
+            await Task.Yield();
+            return Json(content);
+        }
+    }
+}
---
+++ BarCode/Helper/BarCodeHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
+using System.IO;
+namespace BarCode.Helper
+{
+    public class BarCodeHelper
+    {
+        public static async Task GenerateBarCode(string data)
+        {
+            var barCodeData = new BarcodeWriterPixelData
+            {
+                Format = BarcodeFormat.CODE_128,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = 90,
+                    Width = 240,
+                    Margin = 6,
+                    CharacterSet = data
+                }
+            };
+
+            var pixelData = barCodeData.Write(data);
+
+            using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height),
+                        System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+                    System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+
+                    bitmap.UnlockBits(bitmapData);
+                    bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "barcode.png");
+
+                    await File.WriteAllBytesAsync(path, memoryStream.ToArray());
+                }
+            }
+        }
+    }
+}
